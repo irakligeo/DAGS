0e5df40cbf1b04b4d09a2f4f283f4167fb55cb93
developer-- <jemo.mgebrishvili@gmail.com>
2015-07-15 01:19:12 +0400

    init

---
+++ app/src/main/java/geolab/graphitefinder/adpater/ListViewAdapter.java
@@ -0,0 +1,92 @@
+package geolab.graphitefinder.adpater;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import geolab.graphitefinder.R;
+import geolab.graphitefinder.model.GraphiteItemModel;
+
+
+public class ListViewAdapter extends BaseAdapter {
+    private ArrayList<GraphiteItemModel> graphiteItems;
+    private Context context;
+    private LayoutInflater inflater;
+
+
+    public ListViewAdapter(Context context, ArrayList<GraphiteItemModel> graphiteItems){
+        this.context = context;
+        this.graphiteItems = graphiteItems;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return graphiteItems.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return graphiteItems.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int index, View convertView, ViewGroup parentView) {
+
+        View itemView;
+        ViewHolder viewHolder;
+
+        if(convertView == null) {
+            itemView = inflater.inflate(R.layout.fragment_graphiteitemslist,null);
+            viewHolder = new ViewHolder();
+
+            TextView imgTitleView = (TextView) itemView.findViewById(R.id.imgTitle);
+            TextView littleDescriptionView = (TextView) itemView.findViewById(R.id.little_description);
+            ImageView imgView = (ImageView) itemView.findViewById(R.id.peaceOfArtImg);
+
+            viewHolder.imgView = imgView;
+            viewHolder.imgDescriptionView = littleDescriptionView;
+            viewHolder.imgTitleView = imgTitleView;
+
+            itemView.setTag(viewHolder);
+        }
+        else{
+            itemView = convertView;
+            viewHolder = (ViewHolder) itemView.getTag();
+        }
+
+        GraphiteItemModel graphiteItem = (GraphiteItemModel) getItem(index);
+
+        viewHolder.imgTitleView.setText(graphiteItem.getTitle());
+        viewHolder.imgDescriptionView.setText(graphiteItem.getDescription());
+
+        Picasso.with(context)
+                .load(graphiteItem.getImgURL())
+                .resize(200, 200)
+                .centerCrop()
+                .into(viewHolder.imgView);
+
+        return itemView;
+    }
+
+
+    private class ViewHolder {
+        TextView imgTitleView, imgDescriptionView;
+        ImageView imgView;
+    }
+
+
+}
---
+++ app/src/main/java/geolab/graphitefinder/fragment/ItemsListFragment.java
@@ -0,0 +1,66 @@
+package geolab.graphitefinder.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import geolab.graphitefinder.R;
+import geolab.graphitefinder.adpater.ListViewAdapter;
+import geolab.graphitefinder.model.GraphiteItemModel;
+
+
+public class ItemsListFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        Toast.makeText(getActivity(),"Created",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_graphiteitemslist, container, false);
+
+        ArrayList<GraphiteItemModel> listContact = getGraphiteItems();
+        ListView graphiteListView = (ListView)rootView.findViewById(R.id.graphiteList);
+
+        graphiteListView.setAdapter(new ListViewAdapter(getActivity(), listContact));
+
+        return rootView;
+    }
+
+    public ArrayList<GraphiteItemModel> getGraphiteItems(){
+        ArrayList<GraphiteItemModel> graphiteItems = new ArrayList<>();
+
+        graphiteItems.add(new GraphiteItemModel("jemo","blab","sdf","sdfsdF","sdfsdf",13.345,44.3345));
+        graphiteItems.add(new GraphiteItemModel("gio","TTT","sdf","JJJ","OOOO",13.345,44.3345));
+        graphiteItems.add(new GraphiteItemModel("lasha","AAA","sdf","sdfsdF",")))",13.345,44.3345));
+        graphiteItems.add(new GraphiteItemModel("oto","JJJ","sdf","HH234","QQQ",13.345,44.3345));
+        graphiteItems.add(new GraphiteItemModel("lana","LLL","sdf","sdfsdF","sdfsdf",13.345,44.3345));
+
+
+        return graphiteItems;
+
+    }
+
+}
