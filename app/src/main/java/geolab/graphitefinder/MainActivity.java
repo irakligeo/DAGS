7ed2eb4075f4b244c1a25d2c79a8f17b9862a24f
Jemo Mgebrishvili <jemo.mgebrishvili@gmail.com>
2015-07-16 01:58:25 +0400

    nixuia marwyvi

--- app/src/main/java/geolab/graphitefinder/MainActivity.java
+++ app/src/main/java/geolab/graphitefinder/MainActivity.java
@@ -1,40 +1,89 @@
 package geolab.graphitefinder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import geolab.graphitefinder.animation.DepthPageTransformer;
+import geolab.graphitefinder.fragment.MapFragment;
+import geolab.graphitefinder.fragment.TestFrag;
+import geolab.graphitefinder.fragment.ViewPagerFragment;
+
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final int NUM_PAGES = 3;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_screen_slide);
 
-        Intent sliderIntent = new Intent(MainActivity.this, ScreenSlideActivity.class);
-//        Intent sliderIntent = new Intent(MainActivity.this, MapFragment.class);
-        startActivity(sliderIntent);
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+//        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        mPager.setPageTransformer(true, new DepthPageTransformer());
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
+    /**
+     * A simple pager adapter that represents 2 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 1: return MapFragment.newInstance("viewPager");
+                case 2: return TestFrag.newInstance("testFrag");
+
+            }
+            return new ViewPagerFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
+
 }
