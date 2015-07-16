6680cd196e9bd09d6559008509347558030c70d3
Jemo Mgebrishvili <jemo.mgebrishvili@gmail.com>
2015-07-16 17:51:50 +0400

    DB
    added Database

---
+++ app/src/main/java/geolab/graphitefinder/model/DB/TableGraphite.java
@@ -0,0 +1,15 @@
+package geolab.graphitefinder.model.DB;
+
+public class TableGraphite {
+
+    public static String TABLE_NAME = "graphiteItem";
+
+    public static String id;
+    public static String imgTitle;
+    public static String imgAuthor;
+    public static String imgDescription;
+    public static String imgURL;
+    public static double latitude;
+    public static double longitude;
+    public static String uploadDateTime;
+}
