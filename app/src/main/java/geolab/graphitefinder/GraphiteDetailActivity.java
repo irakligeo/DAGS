69bb570236f48e2ef65c1ccee983cf78b28de464
developer-- <jemo.mgebrishvili@gmail.com>
2015-09-04 18:09:55 +0400

    some changes
    blablabla

--- app/src/main/java/geolab/graphitefinder/GraphiteDetailActivity.java
+++ app/src/main/java/geolab/graphitefinder/GraphiteDetailActivity.java
@@ -1,20 +1,25 @@
 package geolab.graphitefinder;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import geolab.graphitefinder.model.GraphiteItemModel;
 
 
-public class GraphiteDetailActivity extends ActionBarActivity {
+public class GraphiteDetailActivity extends ActionBarActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,15 @@ public class GraphiteDetailActivity extends ActionBarActivity {
         //Set Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open,
+                R.string.close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
 
         //get selected item detail
         GraphiteItemModel  graphiteItem = (GraphiteItemModel) getIntent().getSerializableExtra("GraphiteItem");
@@ -53,15 +67,32 @@ public class GraphiteDetailActivity extends ActionBarActivity {
 
     }
 
+    private ActionBarDrawerToggle mDrawerToggle;
 
-
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.navigation_item_1:
+                Toast.makeText(getApplicationContext(), "Item 1", Toast.LENGTH_LONG).show();
+                mDrawerToggle.setHomeAsUpIndicator(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+                break;
+            case R.id.navigation_item_2:
+                Toast.makeText(getApplicationContext(), "Item 2", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_graphite_item_detail, menu);
+
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -72,6 +103,15 @@ public class GraphiteDetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.menu_item_share) {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
             return true;
         }
 
--- app/src/main/java/geolab/graphitefinder/MainActivity.java
+++ app/src/main/java/geolab/graphitefinder/MainActivity.java
@@ -134,7 +134,7 @@ public class MainActivity extends ActionBarActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_graphite_item_detail, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
