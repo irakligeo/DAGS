3cebfbe940d4e1300db44cd5c0238eff0c7ff8f8
developer-- <jemo.mgebrishvili@gmail.com>
2015-09-10 22:51:47 +0400

    done uploading image

--- app/src/main/java/geolab/graphitefinder/fileUpload/Config.java
+++ app/src/main/java/geolab/graphitefinder/fileUpload/Config.java
@@ -5,8 +5,8 @@ package geolab.graphitefinder.fileUpload;
  */
 public class Config {
     // File upload url (replace the ip with your server address)
-    public static final String FILE_UPLOAD_URL = "http://geolab.club/streetart/assets/fileUpload.php";
-
+//    public static final String FILE_UPLOAD_URL = "http://192.168.2.90/fileUpload.php";
+    public static final String FILE_UPLOAD_URL = "http://geolab.club/streetart/fileUpload.php";
     // Directory name to store captured images and videos
     public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
 }
--- app/src/main/java/geolab/graphitefinder/fileUpload/UploadFileActivity.java
+++ app/src/main/java/geolab/graphitefinder/fileUpload/UploadFileActivity.java
@@ -2,16 +2,12 @@ package geolab.graphitefinder.fileUpload;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
